5424529a8437f618d32a521c7964dafff6a2c2ac
Ganga S.Babu <8132694+GangaAjith@users.noreply.github.com>
2022-01-28 19:24:39 +0100

    Splash screen implemented. Theme color changed.

--- app/src/main/java/com/gangaown/shopcar/ui/HomeActivity.kt
+++ app/src/main/java/com/gangaown/shopcar/ui/HomeActivity.kt
@@ -2,12 +2,14 @@ package com.gangaown.shopcar.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.gangaown.shopcar.R
 
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
     }
